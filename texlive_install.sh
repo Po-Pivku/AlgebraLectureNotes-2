d782980ce4fb11365ec0c269ef0293035c9e013a
klimoza <i220303@mail.ru>
2021-02-21 22:32:07 +0300

    Fix Travis #16

--- texlive_install.sh
+++ texlive_install.sh
@@ -25,6 +25,8 @@ fi
 # Needed for any use of texlua even if not testing LuaTeX
 tlmgr install luatex
 
+tlmgr install latexmk
+
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
 tlmgr install   \
