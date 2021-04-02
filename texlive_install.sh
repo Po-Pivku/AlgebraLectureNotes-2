058306023800e756af989d324eb2d34a4bfab279
klimoza <i220303@mail.ru>
2021-04-02 20:52:36 +0300

    Fix Travis

--- texlive_install.sh
+++ texlive_install.sh
@@ -8,7 +8,7 @@
 # required
 
 # See if there is a cached version of TL available
-export PATH=/tmp/texlive/2021/bin/x86_64-linux:$PATH
+export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
@@ -20,8 +20,6 @@ if ! command -v texlua > /dev/null; then
 
   cd ..
 fi
-
-sh update-tlmgr-latest.sh -- --upgrade
 
 tlmgr update --self
 
